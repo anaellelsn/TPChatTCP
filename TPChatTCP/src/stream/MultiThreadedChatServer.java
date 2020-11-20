0ae4970d8e8f721a9d07aa35f9a9d54d1e861bc8
agatheliguori <agathe.liguori@gmail.com>
2020-11-20 17:06:55 +0100

    Implem of history

--- TPChatTCP/src/stream/MultiThreadedChatServer.java
+++ TPChatTCP/src/stream/MultiThreadedChatServer.java
@@ -1,9 +1,20 @@
 package stream;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MultiThreadedChatServer extends Thread {
@@ -47,6 +58,31 @@ public class MultiThreadedChatServer extends Thread {
 				ClientThread ct = new ClientThread(idClients,clientSocket,this);
 				ct.start();
 				threadsClientConnectes.add(ct);
+				try {
+				      File myObj = new File("history.txt");
+				      if (myObj.createNewFile()) {
+				        System.out.println("File created: " + myObj.getName());
+				      } else {
+				        System.out.println("File already exists.");
+				        
+				           
+				        }
+				      File file = new File("history.txt"); 
+				        
+				        BufferedReader br = new BufferedReader(new FileReader(file)); 
+				        
+				        String st; 
+				        while ((st = br.readLine()) != null) {
+				        	
+				        		ct.envoyer(st);
+				        	
+				        	
+				        }
+				      
+				    } catch (IOException e) {
+				      System.out.println("An error occurred.");
+				      e.printStackTrace();
+				    }
 			}
 	    } catch (Exception e) {
 	        
@@ -56,12 +92,27 @@ public class MultiThreadedChatServer extends Thread {
 	public void redirigerMessage(String message, ClientThread client) {
 		for(ClientThread c : threadsClientConnectes) {
 			if(client.getId()!=c.getId()) {
-				System.out.println("redirection de "+client.getId()+" Ã  "+c.getId()+" : "+message);
+				System.out.println("redirection de "+client.getId()+" a  "+c.getId()+" : "+message);
 				c.envoyer("Utilisateur "+c.getId()+" : "+message);
-				
+				DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+				this.history(dateFormat.format(new Date()) + " - Utilisateur "+c.getId()+" : "+message+ System.getProperty("line.separator"), "history.txt");
 			}
 		}
 		
+	}
+	
+	public void history(String message, String fileName) {
+		try { 
+			  
+            // Open given file in append mode. 
+            BufferedWriter out = new BufferedWriter( 
+                   new FileWriter(fileName, true)); 
+            out.write(message); 
+            out.close(); 
+        } 
+        catch (IOException e) { 
+            System.out.println("exception occured" + e); 
+        } 
 	}
 	
 	
